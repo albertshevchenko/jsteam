bdc409848586e3e7601604e3224534f62bbddfb5
albertshevchenko <albertshevchenko@MacBook-Pro-Albert.local>
2016-10-23 20:14:45 +0300

    Component layout
    Верстка компонента «Комментарии», согласно psd-макету.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
+const nested = require('postcss-nested');
 const postcss = require('gulp-postcss');
 const short = require('postcss-short');
 const assets = require('postcss-assets');
-const reporter = require('postcss-browser-reporter');
 const stylelint = require('stylelint');
 const babel = require('gulp-babel');
 
@@ -10,20 +10,20 @@ const rulesStyle = require('./stylelintrc.json');
 
 gulp.task('style', () => {
   const processors = [
+    nested,
     assets,
     short,
-    stylelint(rulesStyle),
-    reporter()
+    stylelint(rulesStyle)
   ];
 
 
-  return gulp.src('./input/css/**/*.css')
+  return gulp.src('./input_files/css/**/main.css')
   .pipe(postcss(processors))
   .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('js', () => {
-  return gulp.src('./input/js/**/*.js')
+  return gulp.src('./input_files/js/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./js/'));
 });
